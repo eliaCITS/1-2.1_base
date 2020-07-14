4011f7bc6ed89ebf725226bae301960fe2aa2f68
Elia Deppe <66836590+eliaCITS@users.noreply.github.com>
2020-07-14 08:40:56 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+example = "example"
+print("This is an", example, "and has no errors.")
+
+name = "Elia Deppe""
+1fav_artist = "Frank Ocean"
+vacation = "Iceland"
+hobbies = "skating," surfing, painting & 
+coding"
+tod@y= """06/20/22"""
+
+print"Today's date is:", tod@y)
+print(">> my name is", nameee, "and my favorite artist is", 1fav_artis)
+print(">> i would love to visit", vacation, "and my hobbies are", hobby)
